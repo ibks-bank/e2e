d997734485eeb2f1f8fa4b935f4a3398131eaf28
Ivan Gavalidi <gavalidi.in@edu.spbstu.ru>
2022-03-16 01:02:50 +0300

    Add basic logger

---
+++ framework/logger.py
@@ -0,0 +1,16 @@
+import logging
+
+
+class Logger:
+    logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
+                        level=logging.INFO, filename=u'logger.log')
+
+    @staticmethod
+    def logging_info_data(sentence):
+        logging.info(sentence)
+
+    @staticmethod
+    def clear_logger():
+        f = open('logger.log', 'r+')
+        f.truncate()
+        f.close()
