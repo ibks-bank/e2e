c30539e28adb5ef9ac0ada6c01c75b4992ff4c97
Ivan Gavalidi <gavalidi.in@edu.spbstu.ru>
2022-03-16 01:03:10 +0300

    Add basic json

---
+++ ui/service_data/read_json.py
@@ -0,0 +1,18 @@
+import json
+
+
+class ReadJson(object):
+    __instance = None
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(ReadJson, cls).__new__(cls)
+        return cls.__instance
+
+    def __init__(self):
+        self.browser = []
+
+    def read_json(self):
+        with open("ui\\service_data\\configuration.json", "r") as read_file:
+            data = json.load(read_file)
+            self.browser = data["browser"]
