9fee96eee3356a640321ff0610457233bb5c8b65
Ivan Gavalidi <gavalidi.in@edu.spbstu.ru>
2022-03-22 23:50:29 +0300

    Fix logger

--- framework/logger.py
+++ framework/logger.py
@@ -2,6 +2,8 @@ import logging
 
 
 class Logger:
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
     logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
                         level=logging.INFO, filename=u'logger.log')
 
