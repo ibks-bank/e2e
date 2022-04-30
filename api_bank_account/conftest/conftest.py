7cff964cc292647a2c31be546c3ea0745803d6dd
Ivan Gavalidi <gavalidi.in@edu.spbstu.ru>
2022-04-30 21:29:47 +0300

    Add tests for bank account

--- api_bank_account/conftest/conftest.py
+++ api_bank_account/conftest/conftest.py
@@ -1,11 +1,79 @@
+import json
+import random
+import string
 import pytest
+import requests
+
 from framework.logger import Logger
-from api_bank_account.service_data.read_json import ReadJson
+from api_profile.service_data.read_json import ReadJson
 
 config = ReadJson()
 config.read_json()
+
+base_url = "http://{ip}:{port}/".format(ip=str(config.ip), port=str(config.port))
 
 
 @pytest.fixture()
 def clear_logger():
     Logger.clear_logger()
+
+
+@pytest.fixture()
+def registration():
+    path = "v1/auth/sign-up"
+    email = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(3)]) + '@' + \
+            ''.join([random.choice(string.ascii_letters + string.digits) for n in range(3)]) + ".com"
+    data = {
+        "email": email,
+        "password": "ibksizi!",
+        "passport": {
+            "series": ''.join(str(item) for item in [random.randint(0, 9) for _ in range(4)]),
+            "number": ''.join(str(item) for item in [random.randint(0, 9) for _ in range(6)]),
+            "firstName": "QA",
+            "middleName": "QA",
+            "lastName": "QA",
+            "issuedBy": "RUSSIA",
+            "issuedAt": "2000-01-01T16:53:20.724Z",
+            "address": "St.Petersburg",
+            "birthplace": "St.Petersburg",
+            "birthdate": "1990-01-01T16:53:20.724Z"
+        }
+    }
+
+    json_data = json.dumps(data, indent=4)
+    response = requests.post(url=base_url + path, json=json.loads(json_data))
+    response_json = json.loads(response.text)
+    assert response.status_code == 200, Logger.logging_info_data("Test 5. FAILED on sign up")
+    user_id = response_json["userID"]
+
+    Logger.logging_info_data("Sign in with new user")
+    path = "v1/auth/sign-in"
+    data = {
+        "email": email,
+        "password": "ibksizi!",
+    }
+    json_data = json.dumps(data, indent=4)
+    response = requests.post(url=base_url + path, json=json.loads(json_data))
+    response_json = json.loads(response.text)
+    assert response.status_code == 200, Logger.logging_info_data("Test 5. FAILED with sign in")
+
+    Logger.logging_info_data("Getting code")
+    path = "v1/qa-api/set-code"
+    data = {
+        "code": "1234",
+        "userID": str(user_id)
+    }
+    json_data = json.dumps(data, indent=4)
+    response = requests.post(url=base_url + path, json=json.loads(json_data))
+    assert response.status_code == 200, Logger.logging_info_data("Test 5. FAILED with internal QA api")
+
+    Logger.logging_info_data("Enter user code")
+    path = "v1/auth/submit-code"
+    data = {
+        "email": email,
+        "password": "ibksizi!",
+        "code": "1234"
+    }
+    json_data = json.dumps(data, indent=4)
+    response = requests.post(url=base_url + path, json=json.loads(json_data))
+    return response
--- test_api_bank_account.py
+++ test_api_bank_account.py
@@ -1,4 +1,8 @@
-from api_profile.service_data.read_json import ReadJson
+import ast
+
+from api_bank_account.conftest.conftest import clear_logger
+from api_bank_account.conftest.conftest import registration
+from api_bank_account.service_data.read_json import ReadJson
 from framework.logger import Logger
 
 import requests
@@ -7,3 +11,140 @@ import random
 
 config = ReadJson()
 config.read_json()
+
+base_url = "http://{ip}:{port}/".format(ip=str(config.ip), port=str(config.port))
+
+
+def test_get_accounts(clear_logger, registration):
+    Logger.logging_info_data("Test 1. Check API get accounts")
+    path = "v1/accounts"
+    response = registration
+    token = response.content
+    token = ast.literal_eval(token.decode('utf-8'))
+    headers = {
+        'X-Auth-Token': str(token['token'])
+    }
+    request = requests.get(url=base_url + path, headers=headers)
+    assert request.status_code == 200, Logger.logging_info_data("Test 1. FAILED with getting accounts")
+    Logger.logging_info_data("Test 1. PASS")
+
+
+def test_successful_create_account(registration):
+    Logger.logging_info_data("Test 2. Check API create account")
+    path = "v1/accounts/create"
+    response = registration
+    token = response.content
+    token = ast.literal_eval(token.decode('utf-8'))
+    headers = {
+        'X-Auth-Token': str(token['token'])
+    }
+    data = {
+        "currency": "CURRENCY_RUB",
+        "limit": "10000",
+        "name": "test"
+    }
+    json_data = json.dumps(data, indent=4)
+    response = requests.post(url=base_url + path, headers=headers, json=json.loads(json_data))
+    assert response.status_code == 200, Logger.logging_info_data("Test 2. FAILED with creating new account")
+    Logger.logging_info_data("Test 2. PASS")
+
+
+def test_unsuccessful_create_account(registration):
+    Logger.logging_info_data("Test 3. Check API unsuccessful create account")
+    path = "v1/accounts/create"
+    response = registration
+    token = response.content
+    token = ast.literal_eval(token.decode('utf-8'))
+    headers = {
+        'X-Auth-Token': str(token['token'])
+    }
+    data = {
+        "currency": "CURRENCY_RUB",
+        "limit": "-10000",
+        "name": "test"
+    }
+    json_data = json.dumps(data, indent=4)
+    response = requests.post(url=base_url + path, headers=headers, json=json.loads(json_data))
+    assert response.status_code == 400, Logger.logging_info_data("Test 2. FAILED with unsuccessful creating new account")
+    Logger.logging_info_data("Test 2. PASS")
+
+
+def test_get_certain_account(registration):
+    path = "v1/accounts/create"
+    response = registration
+    token = response.content
+    token = ast.literal_eval(token.decode('utf-8'))
+    headers = {
+        'X-Auth-Token': str(token['token'])
+    }
+    data = {
+        "currency": "CURRENCY_RUB",
+        "limit": "10000",
+        "name": "test"
+    }
+    json_data = json.dumps(data, indent=4)
+    response = requests.post(url=base_url + path, headers=headers, json=json.loads(json_data))
+    account_id = response.content
+    account_id = ast.literal_eval(account_id.decode('utf-8'))
+    Logger.logging_info_data("Test 4. Check API get certain account")
+    path = "v1/accounts/" + str(account_id['accountID'])
+
+    response = registration
+    token = response.content
+    token = ast.literal_eval(token.decode('utf-8'))
+    headers = {
+        'X-Auth-Token': str(token['token'])
+    }
+    request = requests.get(url=base_url + path, headers=headers)
+    assert request.status_code == 200, Logger.logging_info_data("Test 4. FAILED with getting certain account")
+    Logger.logging_info_data("Test 4. PASS")
+    return str(account_id['accountID']), str(token['token'])
+
+
+def test_fill_account(registration):
+    Logger.logging_info_data("Test 5. Check API fill balance")
+    id, token = test_get_certain_account(registration)
+    path = "v1/accounts/" + str(id) + "/fill-balance"
+    headers = {
+        'X-Auth-Token': token
+    }
+    data = {
+        "accountID": str(id),
+        "amount": "10000"
+    }
+    json_data = json.dumps(data, indent=4)
+    response = requests.post(url=base_url + path, headers=headers, json=json.loads(json_data))
+    assert response.status_code == 200, Logger.logging_info_data("Test 5. FAILED with filling the balance")
+    Logger.logging_info_data("Test 5. PASS")
+
+
+def test_get_transactions(registration):
+    Logger.logging_info_data("Test 6. Check API transactions")
+    path = "v1/transactions"
+    response = registration
+    token = response.content
+    token = ast.literal_eval(token.decode('utf-8'))
+    headers = {
+        'X-Auth-Token': str(token['token'])
+    }
+    request = requests.get(url=base_url + path, headers=headers)
+    assert request.status_code == 200, Logger.logging_info_data("Test 6. FAILED with transactions")
+    Logger.logging_info_data("Test 6. PASS")
+
+
+def test_unsuccessful_create_transactions(registration):
+    Logger.logging_info_data("Test 7. Check API unsuccessful create transaction")
+    id, token = test_get_certain_account(registration)
+    path = "v1/transactions/create"
+    headers = {
+        'X-Auth-Token': token
+    }
+    data = {
+        "accountID": str(id),
+        "payee": str(id),
+        "amount": "10"
+    }
+    json_data = json.dumps(data, indent=4)
+    response = requests.post(url=base_url + path, headers=headers, json=json.loads(json_data))
+    assert response.status_code == 400, Logger.logging_info_data("Test 7. FAILED with unsuccessful creating the transaction")
+    Logger.logging_info_data("Test 7. PASS")
